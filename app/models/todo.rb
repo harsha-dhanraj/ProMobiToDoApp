9c0742c750c33078011463b1108825c7fe535a5e
Harshvardhan <harshu.rtk03@gmail.com>
2018-02-25 22:11:38 +0530

    removed unwanted code

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,15 +4,4 @@ class Todo < ActiveRecord::Base
 	validates_uniqueness_of :task, :scope => :project_id
 	validates_presence_of :task, :scope => :project_id
 	validates_presence_of :developer_id, :scope => :project_id
-	# validate :developer_id_presence
-
-
-	# def developer_id_presence
-	# 	puts "comming hereeeee"
-	# 	if self.developer_id.blank?
-	# 		errors.add(:base, "developer should be present")
-	# 	else
-	# 		puts "nooooooooooooooooooo"
-	# 	end
-	# end
 end
