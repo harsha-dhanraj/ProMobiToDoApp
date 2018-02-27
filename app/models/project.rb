2a9de16111d233153acb37ce49a177321098cec2
Harshvardhan <harshu.rtk03@gmail.com>
2018-02-27 12:49:03 +0530

    Added placeholder for sign in

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,6 @@ class Project < ActiveRecord::Base
 	has_many :todos
   	accepts_nested_attributes_for :todos, allow_destroy: true, reject_if: proc { |attributes| attributes['developer_id'].blank? }
   	has_and_belongs_to_many :users
+
+  	validates_presence_of :name, :description, :client  	
 end
